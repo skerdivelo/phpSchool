834c7df9f5ec3b651314e2271bc15f8637e9416e
Skerdi <81383971+skerdivelo@users.noreply.github.com>
2023-09-22 12:51:06 +0200

    Update script.php

--- script.php
+++ script.php
@@ -1,8 +1,27 @@
 <?php
-    if(isset($_POST["carne"]) || isset($_POST["cipolla"]) || isset($_POST["ketchup"]) || isset($_POST["cetriolini"]) || isset($_POST["patatine"])){
-        echo "Ordine effetuato con successo";
-    }else{
-        header("Location: index.php");
-        exit;
+if (isset($_POST["carne"]) || isset($_POST["cipolla"]) || isset($_POST["ketchup"]) || isset($_POST["cetriolini"]) || isset($_POST["patatine"])) {
+    echo "Ordine effettuato con successo. Elementi selezionati: <br>";
+
+    if (isset($_POST["carne"])) {
+        echo "Carne <br>";
     }
+    if (isset($_POST["cipolla"])) {
+        echo "Cipolla <br>";
+    }
+    if (isset($_POST["ketchup"])) {
+        echo "Ketchup <br>";
+    }
+    if (isset($_POST["insalata"])) {
+        echo "Insalata <br>";
+    }
+    if (isset($_POST["cetriolini"])) {
+        echo "Cetriolini <br>";
+    }
+    if (isset($_POST["patatine"])) {
+        echo "Patatine <br>";
+    }
+} else {
+    header("Location: index.php");
+    exit;
+}
 ?>
